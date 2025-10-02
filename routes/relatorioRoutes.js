a3b0d8ebd2ab21cda3bf96ac4cfdde7c29e13694
Kamolese <162730352+Kamolese@users.noreply.github.com>
2025-10-02 10:03:01 -0300

    Update relatorioRoutes.js

--- routes/relatorioRoutes.js
+++ routes/relatorioRoutes.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { 
-  getRelatorios, 
-  sendReportViaEmail, 
-  sendParticularReportViaEmail, 
-  sendHealthPlanReportViaEmail 
+const {
+  getRelatorios,
+  sendReportViaEmail,
+  sendParticularReportViaEmail,
+  sendHealthPlanReportViaEmail,
+  downloadReportPDF
 } = require('../controllers/relatorioController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.route('/')
-  .get(protect, getRelatorios);
-
-router.route('/email')
-  .post(protect, sendReportViaEmail);
-
-router.route('/email/particular')
-  .post(protect, sendParticularReportViaEmail);
-
-router.route('/email/plano-saude')
-  .post(protect, sendHealthPlanReportViaEmail);
+router.route('/').get(protect, getRelatorios);
+router.route('/email').post(protect, sendReportViaEmail);
+router.route('/email/particular').post(protect, sendParticularReportViaEmail);
+router.route('/email/plano-saude').post(protect, sendHealthPlanReportViaEmail);
+router.route('/download').get(protect, downloadReportPDF);
 
 module.exports = router;
